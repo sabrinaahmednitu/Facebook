78be542f8f241482a551c54e1aae12184e564d79
sabrina <sabrinaahmednitu@gmail.com>
2023-06-22 13:01:08 +0600

    toggle drop down done

---
+++ script.js
@@ -0,0 +1,6 @@
+
+var settingsmenu = document.querySelector('.settings-menu');
+console.log(settingsmenu)
+function settingsMenuToggle() {
+ settingsmenu.classList.toggle('settings-menu-height');
+}

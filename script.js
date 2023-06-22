ffb8b8539c4fbca575bae312e58fde3170ed4e25
sabrina <sabrinaahmednitu@gmail.com>
2023-06-23 00:11:35 +0600

    now light and dark mode will stay if i refresh or reopen after close it

--- script.js
+++ script.js
@@ -11,7 +11,31 @@ var darkbtn = document.getElementById('dark-btn');
 darkbtn.onclick = function () {
   darkbtn.classList.toggle('dark-btn-on');
   // dark background
-  document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('dark-theme');
+    
+    if (localStorage.getItem("theme") == "light") {
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        localStorage.setItem('theme', 'light');
+    }
 }
 
+// dark/white background save on localstorage after refresh
 
+if(localStorage.getItem('theme') == 'light') {
+  darkbtn.classList.remove('dark-btn-on');
+  document.body.classList.remove('dark-theme');
+}
+
+else if(localStorage.getItem('theme') == 'dark') {
+  darkbtn.classList.add('dark-btn-on');
+  document.body.classList.add('dark-theme');
+}
+
+else {
+    localStorage.setItem('theme', 'light');
+}
+
+// localStorage.setItem("theme", "dark");
+// localStorage.setItem("theme");
